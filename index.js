252be4f5d3a94983cece57de02c56c2241dc3c56
mariahgll <mariah.glimah@gmail.com>
2021-11-19 22:41:26 -0300

    JavaScript pronto, botão corrigido no html, e estilização do body.

---
+++ index.js
@@ -0,0 +1,19 @@
+function Get(yourUrl) {
+    var Httpreq = new XMLHttpRequest(); 
+    Httpreq.open("GET", yourUrl, false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+
+var api = JSON.parse(Get('https://619542f674c1bd00176c6ca2.mockapi.io/api/v1/Data'));
+
+    function filtrarData() {
+        const input = document.querySelector('#inputData').value
+        const filtrando = api[0].date.filter((d) => d.data === input)
+        if (filtrando.length != 0) {
+            const resultado = document.getElementById('result').innerHTML = "Data indisponível"
+        }
+        else {
+            const resultado = document.getElementById('result').innerHTML = "Data Liberada, já pode marcar o churrasco!"
+        }
+    }
